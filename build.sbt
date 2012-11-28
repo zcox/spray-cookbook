4048633ac96cef39c431e0d07635c79a7d469413
Zach Cox <zcox522@gmail.com>
2012-11-28 11:42:22 -0600

    brands example

--- build.sbt
+++ build.sbt
@@ -10,14 +10,15 @@ resolvers ++= Seq(
 )
 
 libraryDependencies ++= Seq(
-  "io.spray"          %  "spray-can"       % Version.spray,
-  "io.spray"          %  "spray-routing"   % Version.spray,
-  "io.spray"          %  "spray-testkit"   % Version.spray % "test",
-  "com.typesafe.akka" %  "akka-actor"      % Version.akka,
-  "com.typesafe.akka" %  "akka-slf4j"      % Version.akka,
-  "org.clapper"       %% "grizzled-slf4j"  % "0.6.9",
-  "ch.qos.logback"    %  "logback-classic" % "1.0.6",
-  "org.specs2"        %% "specs2"          % "1.12.2" % "test" //same version as spray-testkit
+  "io.spray"          %  "spray-can"        % Version.spray,
+  "io.spray"          %  "spray-routing"    % Version.spray,
+  "io.spray"          %  "spray-testkit"    % Version.spray % "test",
+  "io.spray"          %  "spray-json_2.9.2" % "1.2.3",
+  "com.typesafe.akka" %  "akka-actor"       % Version.akka,
+  "com.typesafe.akka" %  "akka-slf4j"       % Version.akka,
+  "org.clapper"       %% "grizzled-slf4j"   % "0.6.9",
+  "ch.qos.logback"    %  "logback-classic"  % "1.0.6",
+  "org.specs2"        %% "specs2"           % "1.12.2" % "test" //same version as spray-testkit
 )
 
 scalacOptions += "-Ydependent-method-types" //http://spray.io/documentation/spray-routing/installation/
---
+++ src/main/scala/brands.scala
@@ -0,0 +1,85 @@
+package com.pongr
+
+import akka.actor._
+import akka.dispatch._
+import akka.pattern.ask
+import akka.util.Timeout
+import akka.util.duration._
+import spray.routing._
+import spray.can.server._
+import spray.http._
+import spray.io._
+import spray.json.DefaultJsonProtocol
+import java.util.UUID.{randomUUID => randomUuid}
+
+case class Brand(id: String, name: String, email: String, urlName: String)
+case class FindBrands(query: String)
+case class FoundBrands(brands: Seq[Brand])
+case class CreateBrand(name: String, email: String, urlName: String)
+case class CreatedBrand(brand: Brand)
+
+object BrandJsonProtocol extends DefaultJsonProtocol {
+  /** Allows us to marshall a Brand or a Seq[Brand]. */
+  implicit val BrandFormat = jsonFormat4(Brand)
+}
+
+import BrandJsonProtocol._
+import spray.httpx.SprayJsonSupport._
+
+object Brands {
+  val starbucks = Brand("b1", "Starbucks", "starbucks@pongr.com", "Starbucks")
+  val samsClub = Brand("b2", "Sam's Club", "samsclub@pongr.com", "SamsClub")
+  val pepsi = Brand("b3", "Pepsi", "pepsi@pongr.com", "Pepsi")
+  val square = Brand("b4", "Square", "square@pongr.com", "Square")
+  val all = Seq(starbucks, samsClub, pepsi, square)
+}
+
+class BrandSearchService extends Actor {
+  def receive = {
+    case FindBrands(query) => sender ! FoundBrands(Brands.all.filter(_.name.toLowerCase.startsWith(query.toLowerCase)))
+  }
+}
+
+class BrandService extends Actor {
+  def receive = {
+    //TODO validations: name is unique, email & urlName well-formed, etc
+    case CreateBrand(name, email, urlName) => sender ! CreatedBrand(Brand(randomUuid.toString, name, email, urlName))
+  }
+}
+
+/** Example of the http service deferring all logic to other actors, completing with futures, and marshalling domain objects to json. 
+  * GET /brands?query=s
+  * POST /brands name=New%20Brand&email=newbrand%40.com&urlName=NewBrand
+  */
+trait BrandHttpService extends HttpService {
+  def brandSearchService: ActorRef
+  def brandService: ActorRef
+  implicit val timeout = Timeout(1 second)
+
+  val route = 
+    path("brands") {
+      get {
+        parameter('query).as(FindBrands) { query => 
+          complete {
+            (brandSearchService ? query).mapTo[FoundBrands].map(_.brands)
+          }
+        }
+      } ~ 
+      post {
+        formFields('name, 'email, 'urlName).as(CreateBrand) { command => 
+          complete {
+            (brandService ? command).mapTo[CreatedBrand].map(_.brand)
+          }
+        }
+      }
+    }
+}
+
+object BrandServiceMain extends SprayCanMain {
+  val messageHandler = SingletonHandler(system.actorOf(Props(new Actor with BrandHttpService {
+    def actorRefFactory = context
+    def receive = runRoute(route)
+    val brandSearchService = system.actorOf(Props[BrandSearchService])
+    val brandService = system.actorOf(Props[BrandService])
+  }), "brand-http-service"))
+}
